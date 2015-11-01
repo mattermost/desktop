c2ae7cffb8c588bef12f987d4fb4d02af6259e0a
Yuya Ochiai <yuya0321@gmail.com>
2015-11-02 01:46:51 +0900

    Windows用パッケージにアイコンを設定する

--- src/main.js
+++ src/main.js
@@ -52,7 +52,7 @@ app.on('before-quit', function() {
 app.on('ready', function() {
   // set up tray icon to show balloon
   if (process.platform === 'win32') {
-    trayIcon = new Tray(null);
+    trayIcon = new Tray(__dirname + '/tray.png');
     trayIcon.setToolTip(app.getName());
     ipc.on('notified', function(event, arg) {
       trayIcon.displayBalloon({
