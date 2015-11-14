184f415e951e684bdec140dac9e4e31c844ed609
Yuya Ochiai <yuya0321@gmail.com>
2015-11-14 17:34:25 +0900

    Show window even if it is hidden when notification is clicked

--- src/webview/mattermost.js
+++ src/webview/mattermost.js
@@ -7,6 +7,7 @@ ipc.on('retrieveUnreadCount', function() {
   ipc.sendToHost('retrieveUnreadCount', unreadCount);
 });
 
+// Show balloon when notified.
 if (process.platform === 'win32') {
   Notification = function(title, options) {
     ipc.send('notified', {
@@ -18,4 +19,25 @@ if (process.platform === 'win32') {
     callback('granted');
   };
   Notification.prototype.close = function() {};
+}
+
+// Show window even if it is hidden when notification is clicked.
+var NativeNotification = null;
+if (process.platform === 'darwin') {
+  NativeNotification = Notification;
+  Notification = function(title, options) {
+    this.notification = new NativeNotification(title, options);
+  };
+  Notification.requestPermission = function(callback) {
+    callback('granted');
+  };
+  Notification.prototype.close = function() {
+    this.notification.close();
+  };
+  Notification.prototype.__defineSetter__('onclick', function(callback) {
+    this.notification.onclick = function() {
+      require('remote').getCurrentWindow().show();
+      callback();
+    };
+  });
 }
