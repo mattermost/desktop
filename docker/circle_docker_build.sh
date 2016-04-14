81981e18ae342978e2d247579c5665263a288122
Yuya Ochiai <yuya0321@gmail.com>
2016-04-14 23:59:24 +0900

    Fix the script

--- docker/circle_docker_build.sh
+++ docker/circle_docker_build.sh
@@ -4,10 +4,10 @@ set -ex
 CACHE_DIR=~/docker_cache
 if [ -e ${CACHE_DIR}/em-builder.tar ] && md5sum -c ${CACHE_DIR}/dockerfile.md5sum
 then
-  docker load < ~/${CACHE_DIR}/em-builder.tar
+  docker load < ${CACHE_DIR}/em-builder.tar
 else
   mkdir -p ${CACHE_DIR}
   docker build -t yuya-oc/em-builder docker
-  md5sum Dockerfile > ${CACHE_DIR}/dockerfile.md5sum
+  md5sum docker/Dockerfile > ${CACHE_DIR}/dockerfile.md5sum
   docker save yuya-oc/em-builder > ${CACHE_DIR}/em-builder.tar
 fi
