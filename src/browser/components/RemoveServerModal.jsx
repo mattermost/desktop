ee2e315199d11c79f3dd5aff93476c1f5ad05ade
Yuya Ochiai <yuya0321@gmail.com>
2017-01-24 21:06:07 +0900

    Add "Remove Server" modal

---
+++ src/browser/components/RemoveServerModal.jsx
@@ -0,0 +1,32 @@
+const React = require('react');
+const {Modal} = require('react-bootstrap');
+const DestructiveConfirmationModal = require('./DestructiveConfirmModal.jsx');
+
+function RemoveServerModal(props) {
+  const {serverName, ...rest} = props;
+  return (
+    <DestructiveConfirmationModal
+      {...rest}
+      title='Remove Server'
+      acceptLabel='Remove'
+      cancelLabel='Cancel'
+      body={(
+        <Modal.Body>
+          <p>
+            {'This will remove the server from your Desktop App but will not delete any of its data' +
+          ' - you can add the server back to the app at any time.'}
+          </p>
+          <p>
+            {'Confirm you wish to remove the '}<strong>{serverName}</strong>{' server?'}
+          </p>
+        </Modal.Body>
+      )}
+    />
+  );
+}
+
+RemoveServerModal.propTypes = {
+  serverName: React.PropTypes.string.isRequired
+};
+
+module.exports = RemoveServerModal;
