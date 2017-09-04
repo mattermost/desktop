dab874a8f8570fe303b36ea71e40d996654efc8f
Yuya Ochiai <yuya0321@gmail.com>
2017-09-04 21:25:31 +0900

    Fix cp_artifacts.sh

--- scripts/cp_artifacts.sh
+++ scripts/cp_artifacts.sh
@@ -7,8 +7,8 @@ DEST=$2
 
 cp "${SRC}/mattermost-${VERSION}-win-ia32.zip" "${DEST}/mattermost-desktop-${VERSION}-win-ia32.zip"
 cp "${SRC}/mattermost-${VERSION}-win-x64.zip" "${DEST}/mattermost-desktop-${VERSION}-win-x64.zip"
-cp "${SRC}/squirrel-windows/mattermost-setup-${VERSION}.exe" "${DEST}/mattermost-setup-${VERSION}-x64.exe"
-cp "${SRC}"/squirrel-windows-ia32/*.exe "${DEST}/"
+cp "${SRC}/mattermost-${VERSION}-win.exe" "${DEST}/mattermost-desktop-${VERSION}-win.exe"
 cp "${SRC}"/mattermost-desktop-*.zip "${DEST}/"
 cp "${SRC}"/*.tar.gz "${DEST}/"
 cp "${SRC}"/*.deb "${DEST}/"
+cp "${SRC}"/*.yml "${DEST}/"
