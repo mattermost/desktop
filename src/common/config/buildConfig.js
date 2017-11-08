fdf0d51442290badcd270eae728db2c48832d499
Yuya Ochiai <yuya0321@gmail.com>
2017-11-09 00:37:35 +0900

    Update error message for buildConfig

--- src/common/config/buildConfig.js
+++ src/common/config/buildConfig.js
@@ -6,6 +6,8 @@
  * @prop {string} helpLink - The URL for "Help->Learn More..." menu item.
  *                           If null is specified, the menu disappears.
  * @prop {boolean} enableServerManagement - Whether users can edit servers configuration.
+ *                                          Specify at least one server for "defaultTeams"
+ *                                          when "enableServerManagement is set to false
  */
 const buildConfig = {
   defaultTeams: [/*
