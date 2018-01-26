ab6020973e7b6a8b2b2a2b0e73ac383454513cd9
Yuya Ochiai <yuya0321@gmail.com>
2018-01-26 22:18:48 +0900

    Add checksum for release template

--- scripts/generate_release_markdown.sh
+++ scripts/generate_release_markdown.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 set -eu
+
+function print_link() {
+  local URL="${1}"
+  local CHECKSUM="$(curl -s -S -L "${URL}" | sha256sum | awk '{print $1}')"
+  echo "${URL}"
+  echo "  - SHA-256 Checksum: \`${CHECKSUM}\`"
+}
+
 VERSION="$1" # such as 3.7.1, 4.0.0-rc1
+BASE_URL="https://releases.mattermost.com/desktop/${VERSION}"
+
 cat <<-MD
 ### Mattermost Desktop ${VERSION} has been cut!
 The download links can be found below.
 
 #### Linux
-https://releases.mattermost.com/desktop/${VERSION}/mattermost-desktop-${VERSION}-linux-ia32.tar.gz
-https://releases.mattermost.com/desktop/${VERSION}/mattermost-desktop-${VERSION}-linux-x64.tar.gz
-https://releases.mattermost.com/desktop/${VERSION}/mattermost-desktop-${VERSION}-linux-amd64.deb
-https://releases.mattermost.com/desktop/${VERSION}/mattermost-desktop-${VERSION}-linux-i386.deb
+$(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-linux-ia32.tar.gz")
+$(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-linux-x64.tar.gz")
+$(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-linux-amd64.deb")
+$(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-linux-i386.deb")
 
 #### Mac
-https://releases.mattermost.com/desktop/${VERSION}/mattermost-desktop-${VERSION}-osx.tar.gz
+$(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-osx.tar.gz")
 
 #### Windows
-https://releases.mattermost.com/desktop/${VERSION}/mattermost-desktop-${VERSION}-win32.zip
-https://releases.mattermost.com/desktop/${VERSION}/mattermost-desktop-${VERSION}-win64.zip
-https://releases.mattermost.com/desktop/${VERSION}/mattermost-setup-${VERSION}-win32.exe
-https://releases.mattermost.com/desktop/${VERSION}/mattermost-setup-${VERSION}-win64.exe
+$(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-win32.zip")
+$(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-win64.zip")
+$(print_link "${BASE_URL}/mattermost-setup-${VERSION}-win32.exe")
+$(print_link "${BASE_URL}/mattermost-setup-${VERSION}-win64.exe")
 MD
