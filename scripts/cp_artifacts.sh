86743fea436d5ed9900e8d5088a85f961eb79f29
Yuya Ochiai <yuya0321@gmail.com>
2018-06-15 22:21:17 +0900

    Tweak package and artifact name

--- scripts/cp_artifacts.sh
+++ scripts/cp_artifacts.sh
@@ -5,9 +5,9 @@ VERSION=`cat package.json | jq -r '.version'`
 SRC=$1
 DEST=$2
 
-cp "${SRC}/mattermost-${VERSION}-win-ia32.zip" "${DEST}/mattermost-desktop-${VERSION}-win-ia32.zip"
-cp "${SRC}/mattermost-${VERSION}-win-x64.zip" "${DEST}/mattermost-desktop-${VERSION}-win-x64.zip"
-cp "${SRC}/mattermost-${VERSION}-win.exe" "${DEST}/mattermost-desktop-${VERSION}-win.exe"
+cp "${SRC}/mattermost-desktop-${VERSION}-win-ia32.zip" "${DEST}/"
+cp "${SRC}/mattermost-desktop-${VERSION}-win-x64.zip" "${DEST}/"
+cp "${SRC}/mattermost-desktop-setup-${VERSION}-win.exe" "${DEST}/"
 cp "${SRC}"/mattermost-desktop-*.zip "${DEST}/"
 cp "${SRC}"/*.tar.gz "${DEST}/"
 cp "${SRC}"/*.deb "${DEST}/"
