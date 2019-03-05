f38c3bc970b9c03f046068a4ce718e1d8eeed77f
Dean Whillier <dean@whillier.com>
2019-03-05 13:16:31 -0500

    remove comment copied from the web

--- src/browser/webview/mattermost.js
+++ src/browser/webview/mattermost.js
@@ -54,7 +54,6 @@ window.addEventListener('load', () => {
   window.spellCheckHandler = new SpellCheckHandler();
   window.spellCheckHandler.attachToInput();
   
-  // Start off as US English, America #1 (lol)
   window.spellCheckHandler.switchLanguage('en-US');
   
   let contextMenuBuilder = new ContextMenuBuilder(window.spellCheckHandler);
