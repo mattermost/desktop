338fff0620cc67364217772fe08ffa951648133c
Guillermo Vaya <guivaya@gmail.com>
2020-01-17 14:09:23 +0100

    [] add afterpack script to set setuid permissions on linux

---
+++ scripts/afterpack.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2016-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import path from 'path';
+
+import spawn from 'electron-notarize';
+
+const SETUID_PERMISSIONS = '4755';
+
+async function afterPack(context) {
+  if (context.electronPlatformName === 'linux') {
+    context.targets.forEach(async (target) => {
+      if (!['appimage', 'snap'].includes(target.name.toLowerCase())) {
+        const result = await spawn('chmod', [SETUID_PERMISSIONS, path.join(context.appOutDir, 'chrome-sandbox')]);
+        if (result.code !== 0) {
+          throw new Error(
+            `Failed to set proper permissions for linux arch on ${target.name}`
+          );
+        }
+      }
+    });
+  }
+}
+
+export default afterPack;
