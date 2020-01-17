cd2f91ce2d1ac031ad2f6bbd7fa69fb2cd7c74a8
Guillermo Vaya <guivaya@gmail.com>
2020-01-17 14:39:47 +0100

    import to require

--- scripts/afterpack.js
+++ scripts/afterpack.js
@@ -1,9 +1,9 @@
 // Copyright (c) 2016-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import path from 'path';
+const {path} = require('path');
 
-import spawn from 'electron-notarize';
+const {spawn} = require('electron-notarize');
 
 const SETUID_PERMISSIONS = '4755';
 
