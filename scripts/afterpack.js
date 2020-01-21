0adc421e1bed044dcbe95a2f7461d19cc91b0cc2
Guillermo Vaya <guivaya@gmail.com>
2020-01-21 19:58:22 +0100

    fix import

--- scripts/afterpack.js
+++ scripts/afterpack.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2016-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-const {path} = require('path');
+const path = require('path');
 
 const {spawn} = require('electron-notarize');
 
