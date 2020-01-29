aa88cac4961ce76682f73ab02b0523be5f929c52
Devin Binnie <devin.binnie@mattermost.com>
2020-01-29 11:02:42 -0500

    [MM-22073] Added new titlebar to Settings page

--- src/browser/settings.jsx
+++ src/browser/settings.jsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-2016 Yuya Ochiai
 // Copyright (c) 2016-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-import {remote} from 'electron';
+import {remote, ipcRenderer} from 'electron';
 
 window.eval = global.eval = () => { // eslint-disable-line no-multi-assign, no-eval
   throw new Error(`Sorry, ${remote.app.getName()} does not support window.eval() for security reasons.`);
@@ -10,13 +10,43 @@ window.eval = global.eval = () => { // eslint-disable-line no-multi-assign, no-e
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Config from '../common/config';
+
 import SettingsPage from './components/SettingsPage.jsx';
 import contextMenu from './js/contextMenu';
 
 contextMenu.setup(remote.getCurrentWindow());
 
+const config = new Config(remote.app.getPath('userData') + '/config.json', remote.getCurrentWindow().registryConfigData);
+
+function getDarkMode() {
+  if (process.platform !== 'darwin') {
+    return config.darkMode;
+  }
+  return null;
+}
+
+function setDarkMode() {
+  if (process.platform !== 'darwin') {
+    const darkMode = Boolean(config.darkMode);
+    config.set('darkMode', !darkMode);
+    return !darkMode;
+  }
+  return null;
+}
+
+function openMenu() {
+  if (process.platform !== 'darwin') {
+    ipcRenderer.send('open-app-menu');
+  }
+}
+
 ReactDOM.render(
-  <SettingsPage/>,
+  <SettingsPage
+    getDarkMode={getDarkMode}
+    setDarkMode={setDarkMode}
+    openMenu={openMenu}
+  />,
   document.getElementById('content')
 );
 
