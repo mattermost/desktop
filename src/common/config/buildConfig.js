4e3daaa9da0f39df7268f3dec1b51ebd2b2eae2a
Guillermo Vayá <guillermo.vaya@mattermost.com>
2020-04-07 16:38:57 +0200

    [MM-23082] Fixes whitescreen issue on app upgrade / GPO / whitelabeled app deployments (#1246) (#1261)
    Automatic Merge

--- src/common/config/buildConfig.js
+++ src/common/config/buildConfig.js
@@ -9,6 +9,7 @@
  * @prop {Object[]} defaultTeams
  * @prop {string} defaultTeams[].name - The tab name for default team.
  * @prop {string} defaultTeams[].url - The URL for default team.
+ * @prop {string} defaultTeams[].order - Sort order for team tabs (0, 1, 2)
  * @prop {string} helpLink - The URL for "Help->Learn More..." menu item.
  *                           If null is specified, the menu disappears.
  * @prop {boolean} enableServerManagement - Whether users can edit servers configuration.
@@ -20,8 +21,8 @@ const buildConfig = {
     {
       name: 'example',
       url: 'https://example.com'
-    }*/
-  ],
+    }
+  */],
   helpLink: 'https://about.mattermost.com/default-desktop-app-documentation/',
   enableServerManagement: true,
   enableAutoUpdater: true,
