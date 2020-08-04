9542041a75cde20f9272c4161b86d8c08bb118f4
Guillermo Vaya <guivaya@gmail.com>
2020-08-04 11:40:21 +0200

    back to webpack

--- webpack.config.renderer.js
+++ webpack.config.renderer.js
@@ -4,14 +4,38 @@
 
 // This file uses CommonJS.
 /* eslint-disable import/no-commonjs */
+'use strict';
 
-module.exports = {
+const path = require('path');
+
+const merge = require('webpack-merge');
+
+const base = require('./webpack.config.base');
+
+module.exports = merge(base, {
+  entry: {
+    index: './src/browser/index.jsx',
+    settings: './src/browser/settings.jsx',
+    updater: './src/browser/updater.jsx',
+    'webview/mattermost': './src/browser/webview/mattermost.js',
+  },
+  output: {
+    path: path.join(__dirname, 'src/browser'),
+    publicPath: 'browser',
+    filename: '[name]_bundle.js',
+  },
   module: {
     rules: [{
       test: /\.(js|jsx)?$/,
       use: {
         loader: 'babel-loader',
       },
+    }, {
+      test: /\.css$/,
+      use: [
+        {loader: 'style-loader'},
+        {loader: 'css-loader'},
+      ],
     }, {
       test: /\.mp3$/,
       use: {
@@ -34,9 +58,21 @@ module.exports = {
       loader: 'file-loader',
       options: {
         name: '[name].[ext]',
+        outputPath: '/../assets/fonts',
+        publicPath: './assets/fonts',
       },
     }],
   },
-};
+  node: {
+    __filename: true,
+    __dirname: true,
+  },
+  target: 'electron-renderer',
+  devServer: {
+    contentBase: path.join(__dirname, 'src'),
+    inline: true,
+    publicPath: '/browser/',
+  },
+});
 
 /* eslint-enable import/no-commonjs */
