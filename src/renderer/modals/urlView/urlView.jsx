790614a5a60685ab3d160d56d8ff56f91739ac57
Devin Binnie <devin.binnie@mattermost.com>
2021-02-10 11:19:03 -0500

    [MM-32570] Use OpenSans as the font for the URL preview modal

--- src/renderer/modals/urlView/urlView.jsx
+++ src/renderer/modals/urlView/urlView.jsx
@@ -1,6 +1,8 @@
 // Copyright (c) 2016-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'renderer/css/modals.css';
 import 'renderer/css/components/HoveringURL.css';
 
 const queryString = window.location.search;
