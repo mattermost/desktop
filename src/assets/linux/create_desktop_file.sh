b00da1b053e8b4fecedd0b6d08567ba9b9ed381e
Guillermo Vayá <guillermo.vaya@mattermost.com>
2021-04-16 19:29:40 +0200

    Update icons (#1545)

---
+++ src/assets/linux/create_desktop_file.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+WORKING_DIR=`pwd`
+THIS_PATH=`readlink -f $0`
+cd `dirname ${THIS_PATH}`
+FULL_PATH=`pwd`
+cd ${WORKING_DIR}
+cat <<EOS > Mattermost.desktop
+[Desktop Entry]
+Name=Mattermost
+Comment=Mattermost Desktop application for Linux
+Exec="${FULL_PATH}/mattermost-desktop"
+Terminal=false
+Type=Application
+Icon=${FULL_PATH}/app_icon.png
+Categories=Network;InstantMessaging;
+EOS
+chmod +x Mattermost.desktop
