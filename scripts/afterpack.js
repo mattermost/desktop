a9c7e71022177a001fc3d427fcd6fb0123667600
Devin Binnie <devin.binnie@mattermost.com>
2021-05-14 12:20:38 -0400

    Fix error on linux afterpack

--- scripts/afterpack.js
+++ scripts/afterpack.js
@@ -2,6 +2,7 @@
 // See LICENSE.txt for license information.
 
 const path = require('path');
+const electron = require('electron');
 
 const {spawn} = require('electron-notarize/lib/spawn.js');
 
@@ -9,27 +10,28 @@ const SETUID_PERMISSIONS = '4755';
 
 const {flipFuses, FuseVersion, FuseV1Options} = require('@electron/fuses');
 
-async function fixSetuid(target) {
-    if (!['appimage', 'snap'].includes(target.name.toLowerCase())) {
-        const result = await spawn('chmod', [SETUID_PERMISSIONS, path.join(context.appOutDir, 'chrome-sandbox')]);
-        if (result.code !== 0) {
-            throw new Error(
-                `Failed to set proper permissions for linux arch on ${target.name}`,
-            );
+function fixSetuid(context) {
+    return async (target) => {
+        if (!['appimage', 'snap'].includes(target.name.toLowerCase())) {
+            const result = await spawn('chmod', [SETUID_PERMISSIONS, path.join(context.appOutDir, 'chrome-sandbox')]);
+            if (result.code !== 0) {
+                throw new Error(
+                    `Failed to set proper permissions for linux arch on ${target.name}`,
+                );
+            }
         }
-    }
+    };
 }
 
 exports.default = async function afterPack(context) {
     await flipFuses(
-        // eslint-disable-next-line global-require
-        require('electron'), // Returns the path to the electron binary
+        electron, // Returns the path to the electron binary
         {
             version: FuseVersion.V1,
             [FuseV1Options.RunAsNode]: false, // Disables ELECTRON_RUN_AS_NODE
         });
 
     if (context.electronPlatformName === 'linux') {
-        context.targets.forEach(fixSetuid);
+        context.targets.forEach(fixSetuid(context));
     }
 };
