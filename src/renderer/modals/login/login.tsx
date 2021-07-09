c00ebec3f3643b6b19206c0b4c3f63606ceec287
Devin Binnie <devin.binnie@mattermost.com>
2021-07-09 09:14:31 -0400

    Refactor into helper

--- src/renderer/modals/login/login.tsx
+++ src/renderer/modals/login/login.tsx
@@ -5,25 +5,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {AuthenticationResponseDetails} from 'electron/renderer';
 
-import {DARK_MODE_CHANGE, GET_DARK_MODE, MODAL_CANCEL, MODAL_RESULT, RETRIEVE_MODAL_INFO} from 'common/communication';
+import {MODAL_CANCEL, MODAL_RESULT, RETRIEVE_MODAL_INFO} from 'common/communication';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'renderer/css/modals.css';
 
-import darkStyles from 'renderer/css/lazy/modals-dark.lazy.css';
+import setupDarkMode from '../darkMode';
 
 import LoginModal from './loginModal';
 
-window.addEventListener('message', async (event) => {
-    if (event.data.type === DARK_MODE_CHANGE) {
-        if (event.data.data) {
-            darkStyles.use();
-        } else {
-            darkStyles.unuse();
-        }
-    }
-});
-window.postMessage({type: GET_DARK_MODE}, window.location.href);
+setupDarkMode();
 
 const handleLoginCancel = (request: AuthenticationResponseDetails) => {
     window.postMessage({type: MODAL_CANCEL, data: {request}}, window.location.href);
