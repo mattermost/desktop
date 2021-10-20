9375e44cf2e0551aa86c5964feb583de47fd4ea2
Devin Binnie <devin.binnie@mattermost.com>
2021-10-20 14:20:57 -0400

    WIP

--- test/specs/index.js
+++ test/specs/index.js
@@ -4,9 +4,11 @@
 import './app_test.js';
 import './config_test.js';
 import './window_test.js';
+
 // import './security_test.js';
 // import './spellchecker_test.js';
 import './browser/index_test.js';
+
 // import './browser/settings_test.js';
 // import './main/user_activity_monitor_test.js';
 // import './utils/util_test.js';
