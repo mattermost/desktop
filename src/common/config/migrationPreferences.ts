fd7f3c1c4ded9bd1991e3b8b612bb41fe79f22c6
Devin Binnie <devin.binnie@mattermost.com>
2021-12-17 13:01:25 -0500

    Lint fix

--- src/common/config/migrationPreferences.ts
+++ src/common/config/migrationPreferences.ts
@@ -1,11 +1,11 @@
 // Copyright (c) 2016-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {Config, MigrationInfo} from 'types/config';
+
 import JsonFileManager from 'common/JsonFileManager';
 
 import {migrationInfoPath} from 'main/constants';
-
-import {Config, MigrationInfo} from 'types/config';
 
 export default function migrateConfigItems(config: Config) {
     const migrationPrefs = new JsonFileManager<MigrationInfo>(migrationInfoPath);
