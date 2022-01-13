84d9e6f91cb3fd59e5e6b3824a9f4cd6c50276b1
Devin Binnie <devin.binnie@mattermost.com>
2022-01-13 13:39:10 -0500

    Testing something since this one is still failing

--- e2e/specs/menu_bar/view_menu.test.js
+++ e2e/specs/menu_bar/view_menu.test.js
@@ -41,13 +41,13 @@ describe('mattermost', function desc() {
         }
     });
 
-    // TODO: enable when we have a server to test against
     it('MM-T813 Control+F should focus the search bar in Mattermost', async () => {
         const loadingScreen = this.app.windows().find((window) => window.url().includes('loadingScreen'));
         await loadingScreen.waitForSelector('.LoadingScreen', {state: 'hidden'});
         const firstServer = this.serverMap[`${config.teams[0].name}___TAB_MESSAGING`].win;
         await env.loginToMattermost(firstServer);
         await firstServer.waitForSelector('#searchBox');
+        console.log('test', firstServer.url());
         await firstServer.press('body', process.platform === 'darwin' ? 'Meta+F' : 'Control+F');
         const isFocused = await firstServer.$eval('#searchBox', (el) => el === document.activeElement);
         isFocused.should.be.true;
