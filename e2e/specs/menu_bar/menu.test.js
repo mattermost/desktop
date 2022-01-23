9c45a75511a9e639a960fa077fbc4266e093c135
Oleg Baltag <online7890@gmail.com>
2022-01-23 03:28:50 +0200

    #1960 test update

--- e2e/specs/menu_bar/menu.test.js
+++ e2e/specs/menu_bar/menu.test.js
@@ -35,6 +35,10 @@ describe('menu/menu', function desc() {
             const mainWindow = this.app.windows().find((window) => window.url().includes('index'));
             mainWindow.should.not.be.null;
 
+            await mainWindow.bringToFront();
+            mainWindow.click('#app');
+            await asyncSleep(1000);
+
             // Settings window should open if Alt works
             robot.keyTap('alt');
             robot.keyTap('enter');
