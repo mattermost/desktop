412a54609af425edd6b96d908590d56bc007c1ff
Harrison Healey <harrisonmhealey@gmail.com>
2022-01-31 16:14:13 -0500

    Fix linting error caused by merge

--- e2e/specs/menu_bar/window_menu.test.js
+++ e2e/specs/menu_bar/window_menu.test.js
@@ -95,7 +95,7 @@ describe('Menu/window_menu', function desc() {
         tabViewButton = await mainView.innerText('.active');
         tabViewButton.should.equal('Channels');
     });
-  
+
     it.skip('MM-T824 should be minimized when keyboard shortcuts are pressed', async () => {
         const browserWindow = await this.app.browserWindow(await this.app.firstWindow());
         const mainWindow = this.app.windows().find((window) => window.url().includes('index'));
