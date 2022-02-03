b39645889cfb45b1b93e4dc95b3a060e361c88e2
chris-nee <lingngeehung@gmail.com>
2022-02-03 22:38:16 +0800

    test: MM-T821 open dev tools for Current Server through menu, Menu Dropdown > View > Developer Tools for Current Server

--- e2e/specs/menu_bar/view_menu.test.js
+++ e2e/specs/menu_bar/view_menu.test.js
@@ -140,5 +140,28 @@ describe('mattermost', function desc() {
             const isWindowTitleDevTools = windowTitle === 'DevTools';
             isWindowTitleDevTools.should.be.true;
         });
+
+        it('MM-T821 should open dev tools for Current Server through menu, View > Developer Tools for Current Server', async () => {
+            const windowLoaded = windowEventPromise(this.app);
+            if (process.platform === 'darwin') {
+                robotKeyTaps(1, 'f2', ['control']);
+                robotKeyTaps(3, 'right');
+                robotKeyTaps(1, 'enter');
+                robotKeyTaps(1, 'up');
+                robotKeyTaps(1, 'enter');
+            } else {
+                await clickThreeDotMenu(this.app);
+                robotKeyTaps(3, 'down');
+                robotKeyTaps(1, 'right');
+                robotKeyTaps(1, 'up');
+                robotKeyTaps(1, 'enter');
+            }
+
+            const window = await windowLoaded;
+            const windowTitle = await window.title();
+
+            const isWindowTitleDevTools = windowTitle === 'DevTools';
+            isWindowTitleDevTools.should.be.true;
+        });
     });
 });
