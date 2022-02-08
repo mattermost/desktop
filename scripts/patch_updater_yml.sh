b6ae83bdcdac655664a0b769aeab99321311b9a2
Devin Binnie <devin.binnie@mattermost.com>
2022-02-08 11:15:12 -0500

    Fix script again so it runs in time

--- scripts/patch_updater_yml.sh
+++ scripts/patch_updater_yml.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-ARCH="${1}"
 VERSION="$(jq -r '.version' <package.json)"
 STABLE_VERSION="$(./node_modules/.bin/semver $VERSION -c)"
 RELEASE_VERSION="${VERSION/$STABLE_VERSION/}"
 RELEASE_VERSION="${RELEASE_VERSION/-/}"
 RELEASE_VERSION="${RELEASE_VERSION%.*}"
 
-if ["$RELEASE_VERSION" == ""]; then
+if [ "$RELEASE_VERSION" == "" ]; then
     RELEASE_VERSION="latest"
 fi
 
 echo "${RELEASE_VERSION}"
-VERSION=$VERSION yq eval -i '.files[].url |= strenv(VERSION) + "/" + .' ./release/${RELEASE_VERSION}${1}.yml
+for i in ./release/${RELEASE_VERSION}*.yml; do
+    VERSION=$VERSION yq eval -i '.files[].url |= strenv(VERSION) + "/" + .' $i
+done
+
