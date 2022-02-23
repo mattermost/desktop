08384a23d8e7367637e0ce566273e6e921949080
Devin Binnie <devin.binnie@mattermost.com>
2022-02-23 17:27:51 -0500

    Comment out broken piece

--- scripts/Makefile.ps1
+++ scripts/Makefile.ps1
@@ -674,7 +674,7 @@ function Run-Build {
     Prepare-Path
     Get-Cert
     Run-BuildId
-    Run-BuildChangelog
+    # Run-BuildChangelog
     Run-BuildElectron
     Run-BuildForceSignature
     Run-BuildLicense
