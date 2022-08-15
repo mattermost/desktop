9faaa61e485ca7c223127097a5c5685d8b1e0cff
Devin Binnie <52460000+devinbinnie@users.noreply.github.com>
2022-08-15 09:07:22 -0400

    [MM-46305] Add reporting code for E2E tests (#2222)
    * E2E reporting
    
    * Test report
    
    * Fix artifacts.js
    
    * Revert "Test report"
    
    This reverts commit f4d44b881a19c0e9d63066807f5cb6b9fe9017ee.
    
    * PR feedback

---
+++ e2e/utils/constants.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+const MOCHAWESOME_REPORT_DIR = './mochawesome-report';
+
+module.exports = {
+    MOCHAWESOME_REPORT_DIR,
+};
