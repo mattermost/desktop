a5cb260219013ca33183142c7318c3e1977dfe18
Tasos Boulis <tboulis@hotmail.com>
2022-09-01 15:05:05 +0300

    Add dark mode styling

--- src/main/views/downloadsDropdownView.ts
+++ src/main/views/downloadsDropdownView.ts
@@ -48,6 +48,7 @@ export default class DownloadsDropdownView {
             transparent: true,
         }});
 
+        this.view.webContents.openDevTools();
         this.view.webContents.loadURL(getLocalURLString('downloadsDropdown.html'));
         this.window.addBrowserView(this.view);
 
