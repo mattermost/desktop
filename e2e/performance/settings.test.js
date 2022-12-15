07bba75b8ce4380aa8ca5b6c030e365b063a8b48
Pantelis Vratsalis <pvratsalis@gmail.com>
2022-12-15 13:59:36 +0200

    More minimal reporter and 2 tests: 1 failing, 1 succeeding

--- e2e/performance/settings.test.js
+++ e2e/performance/settings.test.js
@@ -49,9 +49,27 @@ describe('Settings', function desc() {
             const t1 = performance.now();
             const duration = t1 - t0;
 
-            console.log(`Duration: ${duration}ms`);
-
             duration.should.lessThanOrEqual(500);
+        });
+
+        it('Should happen within expected duration failing example', async () => {
+            const expected = (process.platform === 'win32' || process.platform === 'linux');
+            this.app.evaluate(({ipcMain}, showWindow) => {
+                ipcMain.emit(showWindow);
+            }, SHOW_SETTINGS_WINDOW);
+
+            const t0 = performance.now();
+
+            const settingsWindow = await this.app.waitForEvent('window', {
+                predicate: (window) => window.url().includes('settings'),
+            });
+            await settingsWindow.waitForSelector('.settingsPage.container');
+            await settingsWindow.waitForSelector('#inputAutoStart', {state: expected ? 'attached' : 'detached'});
+
+            const t1 = performance.now();
+            const duration = t1 - t0;
+
+            duration.should.lessThanOrEqual(10);
         });
     });
 });
