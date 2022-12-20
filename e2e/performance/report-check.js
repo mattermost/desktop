e265bd1fc76c8a449f8a689469fd4cd37deb18a9
Tasos Boulis <tboulis@hotmail.com>
2022-12-20 17:52:46 +0200

    Throw error when tests fail

--- e2e/performance/report-check.js
+++ e2e/performance/report-check.js
@@ -13,16 +13,26 @@ function checkReport() {
 
     const slowTests = jsonReport?.passes?.filter((test) => test.speed === 'slow');
 
+    if (jsonReport.failures.length > 0) {
+        console.error('\tSome tests failed:');
+        jsonReport.failures.forEach((test) => {
+            console.warn(`\t- ${test.fullTitle}\n`);
+        });
+        process.exitCode = jsonReport.failures.length;
+        return;
+    }
+
     if (slowTests.length > 0) {
         console.warn('\tSlow tests detected:\n');
         slowTests.forEach((test) => {
             console.warn(`\t- ${test.fullTitle}: (${test.duration}ms)\n`);
         });
         process.exitCode = slowTests.length;
-    } else {
-        console.log('No slow tests detected');
-        process.exitCode = 0;
+        return;
     }
+
+    console.log('No slow tests detected');
+    process.exitCode = 0;
 }
 
 checkReport();
