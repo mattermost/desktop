3f3bda2de4b4e855b5db31027b7983ff32a126b8
Tasos Boulis <tboulis@hotmail.com>
2022-12-21 18:51:15 +0200

    Use measudePerformance function instead

--- e2e/performance/app.test.js
+++ e2e/performance/app.test.js
@@ -6,34 +6,31 @@
 const env = require('../modules/environment');
 const {asyncSleep} = require('../modules/utils');
 
+const {measurePerformance} = require('./utils');
+
 describe('startup/app', function desc() {
     this.timeout(30000);
-    this.slow(3000);
-
-    beforeEach(async () => {
-        env.createTestUserDataDir();
-        env.cleanTestConfig();
-        this.app = await env.getApp();
-    });
-
-    afterEach(async () => {
-        if (this.app) {
-            await this.app.close();
-        }
-        await env.clearElectronInstances();
-    });
 
     it('should show the welcome screen modal when no servers exist', async () => {
-        if (this.app) {
-            await this.app.close();
-        }
-        await env.clearElectronInstances();
-        env.createTestUserDataDir();
-        env.cleanTestConfig();
-        this.app = await env.getApp();
-        await asyncSleep(500);
-        const welcomeScreenModal = this.app.windows().find((window) => window.url().includes('welcomeScreen'));
-        const modalButton = await welcomeScreenModal.innerText('.WelcomeScreen .WelcomeScreen__button');
-        modalButton.should.equal('Get Started');
+        await measurePerformance(
+            async () => {
+                if (this.app) {
+                    await this.app.close();
+                }
+                await env.clearElectronInstances();
+                env.createTestUserDataDir();
+                env.cleanTestConfig();
+            },
+            async () => {
+                this.app = await env.getApp();
+                await asyncSleep(500);
+                const welcomeScreenModal = this.app.windows().find((window) => window.url().includes('welcomeScreen'));
+                const modalButton = await welcomeScreenModal.innerText('.WelcomeScreen .WelcomeScreen__button');
+                modalButton.should.equal('Get Started');
+            },
+            () => {},
+            5,
+            2200,
+        );
     });
 });
