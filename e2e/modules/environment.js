21d1192f7e7ec08fb52a27cd657600e49b2cd203
Tasos Boulis <tboulis@hotmail.com>
2023-01-05 12:39:43 +0200

    Revert "Increase retries"
    This reverts commit 564ac95c34def892595c17b98eec245cacb0d4a4.

--- e2e/modules/environment.js
+++ e2e/modules/environment.js
@@ -120,13 +120,13 @@ const demoMattermostConfig = {
 
 const cmdOrCtrl = process.platform === 'darwin' ? 'command' : 'control';
 
-async function findMainWindow(app, depth = 60) {
+async function findMainWindow(app, depth = 10) {
     const mainWindow = app?.windows().find((window) => window.url().includes('index'));
     if (depth === 0) {
         return mainWindow;
     }
     if (!mainWindow) {
-        await asyncSleep(1000);
+        await asyncSleep(500);
         return findMainWindow(app, depth - 1);
     }
     return mainWindow;
