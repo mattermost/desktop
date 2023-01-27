c4293c59a267fc68f9f429ff01f8815b31189e6d
Tasos Boulis <tboulis@hotmail.com>
2023-01-27 16:55:22 +0200

    Remove spaces from line start

--- e2e/utils/pr-e2e-durations-report.js
+++ e2e/utils/pr-e2e-durations-report.js
@@ -2,28 +2,29 @@
 // See LICENSE.txt for license information.
 
 function generateCommentBody(fileContents) {
+    const data = JSON.parse(fileContents);
     return `
-        E2E Performance Test results:
+E2E Performance Test results:
 
-        | Test | Duration |
-        | --- | --- |
-        ${fileContents?.passes?.forEach((pass) => `| ${pass.fullTitle} | ${pass.duration}`)}
+| Test | Duration |
+| --- | --- |
+${data?.passes?.forEach((pass) => `| ${pass.fullTitle} | ${pass.duration}`)}
 
-        ${fileContents.failures?.length > 0 ? `
-            Some tests failed:
-            | Test | Duration |
-            | --- | --- |
-            ${fileContents?.failures?.forEach((pass) => `| ${pass.fullTitle} | ${pass.duration}`)}
-        ` : ''}
+${data?.failures?.length > 0 ? `
+    Some tests failed:
+    | Test | Duration |
+    | --- | --- |
+    ${data.failures.forEach((pass) => `| ${pass.fullTitle} | ${pass.duration}`)}
+` : ''}
 
-        <details>
-            <summary>Raw results</summary>
+<details>
+<summary>Raw results</summary>
 
-            \`\`\`js
-                ${fileContents}
-            \`\`\`
-        </details>
-    `;
+\`\`\`js
+${fileContents}
+\`\`\`
+</details>
+`;
 }
 
 module.exports = {
