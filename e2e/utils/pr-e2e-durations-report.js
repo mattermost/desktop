879dfcdec8908e23c7fde1876d897c7d04a3484e
Tasos Boulis <tboulis@hotmail.com>
2023-01-27 17:08:55 +0200

    Fix markdown table data

--- e2e/utils/pr-e2e-durations-report.js
+++ e2e/utils/pr-e2e-durations-report.js
@@ -3,20 +3,22 @@
 
 function generateCommentBody(fileContents) {
     const data = JSON.parse(fileContents);
-    console.log({fileContents, data});
 
     return `
 E2E Performance Test results:
 
 | Test | Duration |
 | --- | --- |
-${data?.passes?.forEach((pass) => `| ${pass.fullTitle || 'title'} | ${pass.duration}ms |`)}
+${data?.passes?.reduce((acc, pass) => {
+        return `${acc}| ${pass.fullTitle || 'title'} | ${pass.duration}ms |\n`;
+    }, '')
+};
 
 ${data?.failures?.length > 0 ? `
     Some tests failed:
     | Test | Duration |
     | --- | --- |
-    ${data.failures.forEach((pass) => `| ${pass.fullTitle} | ${pass.duration}`)}
+    ${data.failures.forEach((failure) => `| ${failure.fullTitle} | ${failure.duration}`)}
 ` : ''}
 
 <details>
