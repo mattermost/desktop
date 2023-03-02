a52e96694dd5b2be836c5b260e234810273f5c3e
Claudio Costa <cstcld91@gmail.com>
2023-03-02 12:10:47 -0600

    [MM-50973] Harden Calls windows URL navigation checks (#2573)
    * Harden Calls windows URL navigation checks
    
    * Update src/main/windows/callsWidgetWindow.ts
    
    Co-authored-by: Eva Sarafianou <eva.sarafianou@gmail.com>
    
    * Fix bad merge
    
    * Extract team name to use isUrlType
    
    * Simplify
    
    ---------
    
    Co-authored-by: Eva Sarafianou <eva.sarafianou@gmail.com>

--- src/main/windows/callsWidgetWindow.ts
+++ src/main/windows/callsWidgetWindow.ts
@@ -30,7 +30,6 @@ import {
     CALLS_WIDGET_SHARE_SCREEN,
 } from 'common/communication';
 import webContentsEventManager from 'main/views/webContentEvents';
-import Config from 'common/config';
 
 type LoadURLOpts = {
     extraHeaders: string;
@@ -82,6 +81,10 @@ export default class CallsWidgetWindow extends EventEmitter {
         this.win.webContents.setWindowOpenHandler(this.onPopOutOpen);
         this.win.webContents.on('did-create-window', this.onPopOutCreate);
 
+        // Calls widget window is not supposed to navigate anywhere else.
+        this.win.webContents.on('will-navigate', this.onNavigate);
+        this.win.webContents.on('did-start-navigation', this.onNavigate);
+
         this.load();
     }
 
@@ -100,6 +103,14 @@ export default class CallsWidgetWindow extends EventEmitter {
 
     public getCallID() {
         return this.config.callID;
+    }
+
+    private onNavigate = (ev: Event, url: string) => {
+        if (url === this.getWidgetURL()) {
+            return;
+        }
+        log.warn(`CallsWidgetWindow: prevented widget window from navigating to: ${url}`);
+        ev.preventDefault();
     }
 
     private load() {
@@ -209,22 +220,25 @@ export default class CallsWidgetWindow extends EventEmitter {
         this.setBounds(initialBounds);
     }
 
-    private onPopOutOpen = () => {
-        return {
-            action: 'allow' as const,
-            overrideBrowserWindowOptions: {
-                autoHideMenuBar: true,
-            },
-        };
+    private onPopOutOpen = ({url}: {url: string}) => {
+        if (urlUtils.isCallsPopOutURL(this.mainView.serverInfo.server.url, url, this.config.callID)) {
+            return {
+                action: 'allow' as const,
+                overrideBrowserWindowOptions: {
+                    autoHideMenuBar: true,
+                },
+            };
+        }
+
+        log.warn(`CallsWidgetWindow.onPopOutOpen: prevented window open to ${url}`);
+        return {action: 'deny' as const};
     }
 
     private onPopOutCreate = (win: BrowserWindow) => {
         this.popOut = win;
 
         // Let the webContentsEventManager handle links that try to open a new window
-        const spellcheck = Config.useSpellChecker;
-        const newWindow = webContentsEventManager.generateNewWindowListener(this.popOut.webContents.id, spellcheck);
-        this.popOut.webContents.setWindowOpenHandler(newWindow);
+        webContentsEventManager.addWebContentsEventListeners(this.popOut.webContents);
     }
 
     private onPopOutFocus = () => {
@@ -239,6 +253,10 @@ export default class CallsWidgetWindow extends EventEmitter {
 
     public getWebContentsId() {
         return this.win.webContents.id;
+    }
+
+    public getPopOutWebContentsId() {
+        return this.popOut?.webContents.id;
     }
 
     public getURL() {
