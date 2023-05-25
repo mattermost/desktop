8c580ff45687ffa9e87f377398e995d01a892762
Devin Binnie <52460000+devinbinnie@users.noreply.github.com>
2023-05-25 10:03:53 -0400

    [MM-51092] Automate release post for RCs and finals, include changelog (#2733)

---
+++ scripts/generate_release_post.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eu
+
+VERSION="$1" # such as 5.3.0-rc.1, 5.0.0
+LAST_VERSION="$(git for-each-ref --sort=creatordate --format '%(refname)' refs/tags | grep "v[0-9]\.[0-9]\.[0-9]" | grep -v mas | tail -2 | head -1 | sed "s/refs\/tags\/v//")"
+
+cat <<-MD
+### [v$VERSION](https://github.com/mattermost/desktop/releases/tag/v$VERSION) :tada:
+Changes:
+$(git cherry -v v$LAST_VERSION | grep ^+ | grep "(#[0-9]\+)" | sed "s/^+\s[a-zA-Z0-9]\+\s/- /" | sed "s/\s(#\([0-9]\+\))$/ [(#\1)](https:\/\/github.com\/mattermost\/desktop\/pull\/\1)/" | sed "s/\[\?MM-\([0-9]\+\)\]\?/[[MM-\1]](https:\/\/mattermost.atlassian.net\/browse\/MM-\1)/")
+
+The release will be available on GitHub shortly.
+MD
