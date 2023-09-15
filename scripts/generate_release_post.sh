7ad0976dea5b492a0be7bc9f43311f5263abc41f
Devin Binnie <52460000+devinbinnie@users.noreply.github.com>
2023-09-15 12:56:45 -0400

    Revert "Add debugging for notification script (#2840)" (#2842)
    This reverts commit 55fe6ecec99e9fa2cdfd0c077769c8f54c7e8a39.
    
    (cherry picked from commit 008553bd41708198dad8d7a929591a6084c831f2)

--- scripts/generate_release_post.sh
+++ scripts/generate_release_post.sh
@@ -5,15 +5,10 @@ VERSION="$1" # such as 5.3.0-rc.1, 5.0.0
 
 TEMP_VERSION_FILE="$(mktemp -t temp_version_file.XXXX)"
 git for-each-ref --sort=creatordate --format '%(refname)' refs/tags | grep "v[0-9]\.[0-9]\.[0-9]" | grep -v mas | grep -v "v$VERSION" | sed "s/refs\/tags\/v//" > $TEMP_VERSION_FILE
-
-# Debugging
-cat $TEMP_VERSION_FILE
-
 LAST_VERSION="$(cat $TEMP_VERSION_FILE | tail -1)"
+
 TEMP_CHANGES_FILE="$(mktemp -t temp_changes_file.XXXX)"
 git cherry -v v$LAST_VERSION v$VERSION | grep ^+ | grep "(#[0-9]\+)" > $TEMP_CHANGES_FILE
-
-cat $TEMP_CHANGES_FILE
 
 cat <<-MD
 ### [v$VERSION](https://github.com/mattermost/desktop/releases/tag/v$VERSION) :tada:
