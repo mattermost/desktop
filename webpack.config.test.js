4767e329ed5841f50299f91a42f9b096ecfe07bb
yasserfaraazkhan <attitude3cena.yf@gmail.com>
2024-03-13 20:36:05 +0530

    test-1

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -14,7 +14,7 @@ const base = require('./webpack.config.base');
 
 module.exports = merge(base, {
     entry: {
-        e2e: glob.sync('./e2e/specs/**/*.test.js'),
+        e2e: glob.sync('./e2e/specs/**/view_menu.test.js'),
     },
     output: {
         path: path.resolve(__dirname, 'dist/tests'),
