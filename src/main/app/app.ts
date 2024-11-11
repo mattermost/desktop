d0355cba4a480d7e492662f96a1cb41a5d6bfc7c
Devin Binnie <devin.binnie@mattermost.com>
2024-11-11 15:52:50 -0500

    Don't call show() before the window is created on Windows

--- src/main/app/app.ts
+++ src/main/app/app.ts
@@ -29,7 +29,6 @@ export function handleAppSecondInstance(event: Event, argv: string[]) {
 
     // Protocol handler for win32
     // argv: An array of the second instance’s (command line / deep linked) arguments
-    MainWindow.show();
     const deeplinkingURL = getDeeplinkingURL(argv);
     if (deeplinkingURL) {
         openDeepLink(deeplinkingURL);
