2556ddf1d660e9857ada9784a71e0adaf1c2c541
yasserfaraazkhan <attitude3cena.yf@gmail.com>
2025-04-30 12:22:21 +0530

    add preload test script

--- e2e/run-tests.sh
+++ e2e/run-tests.sh
@@ -22,7 +22,7 @@ export ELECTRON_EXTRA_LAUNCH_ARGS="--disable-gpu --no-sandbox"
 
 # Run the tests with proper error handling
 cd e2e
-NODE_ENV=development electron-mocha --require-main ./preload.js --no-sandbox --renderer --reporter mochawesome dist/e2e_bundle.js || true
+NODE_ENV=development ./node_modules/.bin/electron-mocha --require-main ./preload.js --no-sandbox --renderer --reporter mochawesome dist/e2e_bundle.js || true
 
 npm run send-report || echo "Failed to send report"
 
