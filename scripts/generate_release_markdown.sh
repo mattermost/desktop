1641511a6c520c4e33584717fe874ccd21b0bc69
Devin Binnie <devin.binnie@mattermost.com>
2025-12-19 10:18:26 -0500

    Drop beta label from ARM64

--- scripts/generate_release_markdown.sh
+++ scripts/generate_release_markdown.sh
@@ -23,11 +23,11 @@ The download links can be found below.
 
 #### Windows - installer files
 $(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-win-x64.msi")
-$(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-win-arm64.msi") (beta)
+$(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-win-arm64.msi")
 
 #### Windows - zip files
 $(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-win-x64.zip")
-$(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-win-arm64.zip") (beta)
+$(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-win-arm64.zip")
 
 #### Mac
 $(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-mac-universal.dmg")
@@ -35,18 +35,18 @@ $(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-mac-x64.dmg")
 $(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-mac-m1.dmg")
 
 #### Linux
-$(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-linux-arm64.tar.gz") (beta)
+$(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-linux-arm64.tar.gz")
 $(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-linux-x64.tar.gz")
 
 #### Linux (Unofficial) - deb files
-$(print_link "${BASE_URL}/mattermost-desktop_${VERSION}-1_arm64.deb") (beta)
+$(print_link "${BASE_URL}/mattermost-desktop_${VERSION}-1_arm64.deb")
 $(print_link "${BASE_URL}/mattermost-desktop_${VERSION}-1_amd64.deb")
 
 #### Linux (Unofficial) - rpm files (beta)
-$(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-linux-aarch64.rpm") (beta)
+$(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-linux-aarch64.rpm")
 $(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-linux-x86_64.rpm")
 
 #### Linux (Unofficial) - AppImage files
-$(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-linux-arm64.AppImage") (beta)
+$(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-linux-arm64.AppImage")
 $(print_link "${BASE_URL}/mattermost-desktop-${VERSION}-linux-x86_64.AppImage")
 MD
