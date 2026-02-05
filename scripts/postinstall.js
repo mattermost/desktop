75a5fe592f9ada42f6cb23b8f5cb209496567b74
Devin Binnie <devin.binnie@mattermost.com>
2026-02-05 08:28:38 -0500

    Fix lint

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -7,14 +7,14 @@ const SETUID_PERMISSIONS = '4755';
 
 if (process.platform === 'linux' && !process.env.CI) {
     console.log('Setting proper ownership for linux arch');
-    var result = spawnSync('sudo', ['chown', 'root:root', './node_modules/electron/dist/chrome-sandbox']);
+    let result = spawnSync('sudo', ['chown', 'root:root', './node_modules/electron/dist/chrome-sandbox']);
     if (result.error) {
         throw new Error(
             `Failed to set proper ownership for linux arch: ${result.error} ${result.stderr} ${result.stdout}`,
         );
     }
 
-    var result = spawnSync('sudo', ['chmod', SETUID_PERMISSIONS, './node_modules/electron/dist/chrome-sandbox']);
+    result = spawnSync('sudo', ['chmod', SETUID_PERMISSIONS, './node_modules/electron/dist/chrome-sandbox']);
     if (result.error) {
         throw new Error(
             `Failed to set proper permissions for linux arch: ${result.error} ${result.stderr} ${result.stdout}`,
